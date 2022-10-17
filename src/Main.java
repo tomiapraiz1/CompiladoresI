6fe35a15036d10fb1fbc2d91815af9b0e808d0ef
tomi <tapraiz@alumnos.exa.unicen.edu.ar>
2022-10-17 16:02:01 -0300

    entrega final con archivo .jar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 import AnalizadorLexico.*;
 import AnalizadorSintactico.*;
@@ -13,13 +14,22 @@ public class Main {
 				String pathA = "src/AnalizadorLexico/MatrizAcciones.txt";
 				String pathS = "src/AnalizadorLexico/MatrizEstados.txt";
 				AnalizadorLexico l = new AnalizadorLexico(pathS, pathA, 15, 26);
-				String prueba = "src/test/pruebaLexico1.txt";
+				System.out.println("Ingrese la direccion del archivo de prueba:");
+				Scanner s = new Scanner(System.in);
+				String prueba = s.nextLine();
 				AnalizadorLexico.r = new BufferedReader(new FileReader(prueba));
 				Parser p = new Parser();
 				p.run();
 				TablaSimbolos.imprimirTabla();
 			} catch (Exception e) {
 				e.printStackTrace();
+			} finally {
+				try {
+					if (AnalizadorLexico.r != null)
+						AnalizadorLexico.r.close();						
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
 			}
 			
 	}
